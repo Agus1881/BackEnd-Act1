3c4c2b111655dd12f871a9fd7ab69bfb5224a94f
Agustin Carril <agustincarril1881@gmail.com>
2023-10-03 00:39:07 -0300

    segundo commit

--- actividad1.js
+++ actividad1.js
@@ -1,18 +1,41 @@
-function addProduct(title, description, price, thumbnail, code, stock){
-    if(!title, !description, !price, !thumbnail, !code, !stock){
-        console.error("se requiere completar los campos")
-        return
+class ProductManager {
+    constructor(){
+        this.products = []
     }
 
-    const newProduct = {
-        title,
-        description,
-        price,
-        thumbnail,
-        code: codeid + 1,
-        stock,
+    addProduct(title, description, price, thumbnail, code, stock){
+        if(!title, !description, !price, !thumbnail, !code, !stock){
+            console.error("se requiere completar los campos")
+            return
+        }
+
+        const newProduct = {
+            id: id++,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        }
+
+        const productCode = this.products.find((item) => item.code === code)
+
+        if(productCode){
+            console.error("error, ese codigo ya existe.")
+            return
+        }
+        
+        this.products.push(newProduct)
+        console.log("producto añadido")
     }
 }
+
+const ProductManager = new ProductManager()
+
+ProductManager.addProduct(1, remera, ropa, 3000, imagen, a1, 7)
+
+console.log(products.getProducts())
 
 function getProducts(){
     return this.products
